8b1c4a5e6a242a35a4d9390476ff906bd8e2eb7c
Joseph Montalto <joey.montalto@hotmail.com>
2023-11-06 09:47:29 -0500

    Clear out all files

--- Final/web/README.txt
+++
@@ -1,2 +0,0 @@
-The working app is available online at
-    https://daniels.sgedu.site/Final/index.php
--- Final/web/signup.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/src/setup.php';
-
-$page = new EasyReader\Pages\SignUpPage();
-
-echo $page->getOutput();
